0ad69b9b6b77e2efc02f7498e70a016b90fe9905
jkurnia <julia@zidisha.org>
2018-02-06 16:32:02 -0500

    edit about copy

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -27,14 +27,14 @@ About
             </div>
             <div class="col-lg-6">
                 <h2>About our school</h2>
-                <p>Our methodology is to have youth develop a superior level of spiritual,  physical and mental fitness. Students are motivated to learn in a positive and safe atmosphere of encouragement and fun. Discipline is taught in a firm and loving manner. Students who participate in the program have increased self-confidence, focus, discipline and physical fitness. Students are rewarded for mastering Silat techniques as well as accomplishments in school and other areas of life.</p>
-                <p>Pencak Silat or Silat (pronounced PUN-chah SEE-laht) is an Indonesian martial art rooted in Islam and the culture of the Malay people. It is an exceptionally rich martial arts tradition, with hundreds of styles practiced throughout the Indonesian archipelago. Silat is known for its beauty, fluidity and fighting efficacy. Many of its techniques are inspired by the movements of animals such as the tiger, eagle, monkey and snake.  Silat training encompasses four dimensions:</p>
+                <p>Pencak Silat (pronounced PUN-chah SEE-laht) or Silat is an Indonesian martial art known for its beauty, fluidity and fighting efficacy. It is an exceptionally rich martial arts tradition, with hundreds of styles practiced throughout the Indonesian archipelago. Many of its techniques are inspired by the movements of animals such as the tiger, eagle, monkey and snake.  Silat training encompasses four dimensions:</p>
                 <ul>
                     <li>Self defense: practical application of self-defense techniques</li>
                     <li>Art: culture and artistic performances of traditional forms</li>
                     <li>Sport: fitness and competition sparring</li>
                     <li>Mental/spiritual: development of a noble character</li>
                 </ul>
+                <p>Our students develop a superior level of spiritual, physical and mental fitness as a result of Silat training. Students are motivated to learn in a positive and safe atmosphere of encouragement and fun, where discipline is taught in a firm and loving manner. Students are rewarded for mastering Silat techniques as well as accomplishments in school and other areas of life.</p>
             </div>
         </div>
         <!-- /.row -->
