0f19df3cdced1aad61a0262765904beb55f3d69b
jkurnia <julia@zidisha.org>
2018-02-06 18:06:22 -0500

    small copy edit

--- resources/views/pages/instructors.blade.php
+++ resources/views/pages/instructors.blade.php
@@ -18,7 +18,7 @@ Instructors
             </div>
             <div class="col-md-5">
                 <h3>Terra Kurnia, Head Instructor</h3>
-                <p>Kak Terra comes from a family of Silat masters and began practicing Silat in Indonesia when he was four years old. He has taught hundreds of youth and helped establish the teaching of Silat in the northern Virginia and DC area. Kak Terra specializes in the Bunga or artistic aspects of Silat and has won many first place trophies in forms division as well as World Champion in breaking. His remarkable flexibility and agility has encouraged many students to also excel in these areas, which is especially important in the art of Pencak Silat. He has a good relationship with many of the local martial arts tournament promoters. His cheerful and fun-loving personality makes him a favorite with the children studying Silat.</p>
+                <p>Kak Terra comes from a family of Silat masters and began practicing Silat in Indonesia when he was four years old. He has taught hundreds of youth and helped establish the teaching of Silat in the northern Virginia and DC area. Kak Terra specializes in the artistic aspects of Silat and has won many first place trophies in forms division and breaking. His remarkable flexibility and agility has encouraged many students to also excel in these areas, which is especially important in the art of Pencak Silat. He has a good relationship with many of the local and national martial arts tournament promoters. His cheerful and fun-loving personality makes him a favorite with the children studying Silat.</p>
             </div>
         </div>
         <!-- /.row -->
