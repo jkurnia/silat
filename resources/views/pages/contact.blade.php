831080da50ff1d7cb2dfa29594d2b5ed88ffcf3f
jkurnia <julia@zidisha.org>
2018-02-09 11:52:44 -0500

    fix typo in zip code

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -30,7 +30,7 @@ Contact
                 <p>
                     44710 Cape Court, Suite 118
                     <br>Second floor
-                    <br>Ashburn, VA 201470
+                    <br>Ashburn, VA 20147
                     <br>
                 </p>
                 <p><a href="https://www.google.com/maps/dir//44710+Cape+Ct+%23118,+Ashburn,+VA+20147/@39.0188564,-77.5235786,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89b63926ac802b03:0xea34fa092b514fb6!2m2!1d-77.4533661!2d39.0188774">Get directions</a></p>
