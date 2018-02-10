6e9aebd50516dcd814562eecaea19e375e0c6fde
jkurnia <julia@zidisha.org>
2018-02-10 10:35:04 -0500

    hide email in mobile to prevent nav bar covering content

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,13 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
         <a class="navbar-brand" href="{{ route('home') }}">Silat Virginia</a>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="mailto:info@silatvirginia.com"><i class="fa fa-envelope"></i> info@silatvirginia.com</a>
-            </li>
-        </ul>
+        <div class="hide-in-mobile">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="mailto:info@silatvirginia.com"><i class="fa fa-envelope"></i> info@silatvirginia.com</a>
+                </li>
+            </ul>
+        </div>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
