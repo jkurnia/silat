d6c55ef07e5361e0c899c586c42cdb8bdeeeee61
jkurnia <julia@zidisha.org>
2018-02-10 11:00:09 -0500

    remove unused html

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -11,15 +11,6 @@ About
         <h1 class="mt-4 mb-3">About Our School
         </h1>
 
-        <!--
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item">
-                <a href="{{ route('home') }}">Home</a>
-            </li>
-            <li class="breadcrumb-item active">About</li>
-        </ol>
-        -->
-
         <!-- Intro Content -->
         <div class="row">
             <div class="col-lg-6">
@@ -40,74 +31,6 @@ About
             </div>
         </div>
         <!-- /.row -->
-
-        <!--
-        <h2>Our Team</h2>
-
-        <div class="row">
-            <div class="col-lg-4 mb-4">
-                <div class="card h-100 text-center">
-                    <img class="card-img-top" src="http://placehold.it/750x450" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Team Member</h4>
-                        <h6 class="card-subtitle mb-2 text-muted">Position</h6>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="#">name@example.com</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card h-100 text-center">
-                    <img class="card-img-top" src="http://placehold.it/750x450" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Team Member</h4>
-                        <h6 class="card-subtitle mb-2 text-muted">Position</h6>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="#">name@example.com</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card h-100 text-center">
-                    <img class="card-img-top" src="http://placehold.it/750x450" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Team Member</h4>
-                        <h6 class="card-subtitle mb-2 text-muted">Position</h6>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="#">name@example.com</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <h2>Our Customers</h2>
-        <div class="row">
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-        </div>
-        -->
 
     </div>
     <!-- /.container -->
--- resources/views/pages/schedule.blade.php
+++ resources/views/pages/schedule.blade.php
@@ -11,24 +11,12 @@ Schedule
         <h1 class="mt-4 mb-3">Class Schedule
         </h1>
 
-        <!--
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item">
-                <a href="{{ route('home') }}">Home</a>
-            </li>
-            <li class="breadcrumb-item active">About</li>
-        </ol>
-        -->
-
         <!-- Intro Content -->
         <div class="row">
             <div class="col-lg-6">
                 <img class="img-fluid rounded mb-4" src="{{ asset('assets/images/sparring.jpg') }}" style="image-orientation: from-image">
             </div>
             <div class="col-lg-6">
-                <!--
-                <h2>Class Schedule</h2>
-                -->
                 Monday
                 <ul>
                     <li>4:30 - 5:30 PM: Beginner Youth</li>
@@ -68,74 +56,6 @@ Schedule
             </div>
         </div>
         <!-- /.row -->
-
-        <!--
-        <h2>Our Team</h2>
-
-        <div class="row">
-            <div class="col-lg-4 mb-4">
-                <div class="card h-100 text-center">
-                    <img class="card-img-top" src="http://placehold.it/750x450" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Team Member</h4>
-                        <h6 class="card-subtitle mb-2 text-muted">Position</h6>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="#">name@example.com</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card h-100 text-center">
-                    <img class="card-img-top" src="http://placehold.it/750x450" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Team Member</h4>
-                        <h6 class="card-subtitle mb-2 text-muted">Position</h6>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="#">name@example.com</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card h-100 text-center">
-                    <img class="card-img-top" src="http://placehold.it/750x450" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Team Member</h4>
-                        <h6 class="card-subtitle mb-2 text-muted">Position</h6>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="#">name@example.com</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <h2>Our Customers</h2>
-        <div class="row">
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-            <div class="col-lg-2 col-sm-4 mb-4">
-                <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-            </div>
-        </div>
-        -->
 
     </div>
     <!-- /.container -->
