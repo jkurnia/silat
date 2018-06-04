5ba9d48d437f15fd3337c3f1338c968c9bcb0b40
jkurnia <julia@zidisha.org>
2018-06-04 07:42:44 -0400

    remove julia from instructors page

--- resources/views/pages/instructors.blade.php
+++ resources/views/pages/instructors.blade.php
@@ -23,22 +23,6 @@ Instructors
         </div>
         <!-- /.row -->
 
-        <hr>
-
-        <!-- Project Two -->
-        <div class="row">
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-fluid rounded mb-3 mb-md-0" src="{{ asset('assets/images/julia.png') }}" alt="">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3>Julia Kurnia</h3>
-                <p>Kak Julia has been practicing martial arts for over a decade.  In addition to Silat, she has trained in Tae Kwon Do, MMA, Aikido and Systema.  Julia teaches women's classes for teenagers and adults.</p>
-            </div>
-        </div>
-        <!-- /.row -->
-
         <br/>
     </div>
     <!-- /.container -->
